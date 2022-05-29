39fbf322ef7ccb0ffd5a55dbb93aed4fdf7a26a0
GraemeAngusJones <86078388+GraemeAngusJones@users.noreply.github.com>
2022-05-29 12:37:08 +0100

    Removed renumeration strategy initialisation from constructor
    Removed initialisation of renumeration strategy from constructor and instead included an explicit setRenumeration method in Employee.

--- app.js
+++ app.js
@@ -5,14 +5,20 @@ import { SalaryStrategy, HourlyStrategy, ContractorStrategy } from './Renumerati
 const reportMonthlyPayment = (name, amount) => console.log(`Pay ${name} £${amount} this month.`);
 
 // Different renumeration strategies
+const employee1 = new Employee("Jane Smith");
 const salaryDetails = { salary: 120000 };
-const employee1 = new Employee("Jane Smith", new SalaryStrategy(salaryDetails));
+const salaryStrategy = new SalaryStrategy(salaryDetails);
+employee1.setRenumerationStrategy(salaryStrategy);
 reportMonthlyPayment(employee1.name, employee1.recoverMonthlyPayment());
 
+const employee2 = new Employee("Bob Jones");
 const hourlyPaidDetails = { hourlyRate: 100, numberOfHours: 176 };
-const employee2 = new Employee("Bob Jones", new HourlyStrategy(hourlyPaidDetails));
+const hourlyPaidStrategy = new HourlyStrategy(hourlyPaidDetails);
+employee2.setRenumerationStrategy(hourlyPaidStrategy);
 reportMonthlyPayment(employee2.name, employee2.recoverMonthlyPayment());
 
+const employee3 = new Employee("Ahmed Rahman");
 const contractorDetails = { fixedMonthlyPayment: 14000 };
-const employee3 = new Employee("Ahmed Rahman", new ContractorStrategy(contractorDetails));
+const contractorStrategy = new ContractorStrategy(contractorDetails);
+employee3.setRenumerationStrategy(contractorStrategy);
 reportMonthlyPayment(employee3.name, employee3.recoverMonthlyPayment());
