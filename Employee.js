4e5ea443c5d674ef4bcaf11a119ce1571e0aa16a
Graeme Jones <48164351+graemeajones@users.noreply.github.com>
2022-05-29 12:37:55 +0100

    Removed renumeration strategy initialisation from constructor (#3)
    Removed initialisation of renumeration strategy from constructor and instead included an explicit setRenumeration method in Employee.
    
    Co-authored-by: GraemeAngusJones <86078388+GraemeAngusJones@users.noreply.github.com>

--- Employee.js
+++ Employee.js
@@ -2,9 +2,12 @@ import { IRenumerationStrategy } from './RenumerationStrategy.js';
 
 export default class Employee {
 
-  constructor(name, renumerationStrategy) {
-    IRenumerationStrategy.implementedBy(renumerationStrategy)
+  constructor(name) {
     this.name = name;
+  }
+
+  setRenumerationStrategy(renumerationStrategy) {
+    IRenumerationStrategy.implementedBy(renumerationStrategy)
     this.renumerationStrategy = renumerationStrategy;
   }
 
